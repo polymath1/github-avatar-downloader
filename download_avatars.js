cb93d46300fbb62889c92f90a7f283956e78254f
polymath1 <vino_sivajothy@hotmail.com>
2019-02-12 05:48:45 +0000

    First commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,21 @@
+var lola = require("./secrets.js");
+
+
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  headers: {
+      'User-Agent': 'request'
+      curl -i -H 'Authorization: token ' https://api.github.com/repos/jquery/jquery/contributors
+    }
+  };
+
+
+
   request(url, function(err, res, body) {
     cb(err, body);
   });
